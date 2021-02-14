e0fcdf539accc69a383a8942ebf6fd835695df21
brian Horakh <brianh@elastic.ventures>
2021-02-14 13:54:17 +1100

    tailwind & links, ready to publish

--- src/components/ContactFF.vue
+++ src/components/ContactFF.vue
@@ -1,31 +1,75 @@
+<!--
+
+Iconify docs: 
+  https://github.com/iconify/iconify/tree/master/packages/vue
+  https://iconify.design/icon-sets/
+
+  npm install --save-dev @iconify-icons/brandico
+  npm install --save-dev @iconify-icons/mdi
+
+-->
+
 <template>
+
+<div class="grid grid-cols-3 gap-4 bg-gray-0">
+    <div class="flex justify-center hover:bg-yellow-200">
+        <a href="https://twitter.com/fungiblefarm" target="_blank">
+        <Icon :icon="icons.twitter" height="48" :style="{}" />
+        </a>
+    </div>
+    <div class="flex justify-center hover:bg-yellow-200">
+        <a href="https://www.linkedin.com/company/fungible-farm" target="_blank">
+        <Icon :icon="icons.linkedin" height="48" :style="{}" />
+        </a>
+    </div>
+    <div class="flex justify-center hover:bg-yellow-200">
+        <a href="mailto://info@fungible.farm?subject=Inquiry" target="_blank">
+        <Icon :icon="icons.email" height="48" :style="{}" />
+        </a>
+    </div>
+    <!--
+    
+    
+    -->
+
+</div>
 </template>
 
 <script lang="ts">
+
 import { defineComponent } from 'vue';  // ?? wtf
 
+import { Icon } from '@iconify/vue';
+// import home from '@iconify-icons/mdi/home';
+import iconTwitter from '@iconify-icons/brandico/twitter-bird';
+import iconLinkedIn from '@iconify-icons/brandico/linkedin-rect';
+import iconEmail from '@iconify-icons/mdi/email';
+
+
 interface State {
+    icons: any;
 }
 
 export default defineComponent({
     name: 'ContactFF',
-
+    components: {
+        Icon
+    }, 
     data: (): State => {
         return {
-
+            icons: {
+                twitter: iconTwitter,
+                linkedin: iconLinkedIn,
+                email: iconEmail
+                }
+            }
         },
-    mounted() {
-
-    },
+    // mounted() {},
     methods: {
 
     }
 
-}
-
-
-
-export default Vue.extend({
-    
 })
+
+
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,13 @@
 <template>
-  <div class="home">
-    <FFLogo />
-  
-    <HelloWorld msg="info (at) fungible.farm"/>
-    <!--
-    https://www.linkedin.com/company/fungible-farm
-    https://twitter.com/fungiblefarm
-    -->
-
-    <Icon :icon="icons.twitter" height="48" :style="{ }" />
-    <Icon :icon="icons.linkedin" height="48" :style="{ }" />
-    <Icon :icon="icons.email" height="48" :style="{  }" />
-
+  <div class="block object-none object-center mx-auto px-8">
+    <FFLogo class="max-xl" />
+    <ContactFF class="max-lg" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ContactFF from '@/components/ContactFF.vue';
 import FFLogo from '@/components/FFLogo.vue';
 // import Counter from '@/components/Counter.vue';
 // import nput from '@/components/nput.vue';
@@ -27,37 +17,17 @@ import FFLogo from '@/components/FFLogo.vue';
   https://medium.com/@FlorianWoelki/vue-3-and-tailwindcss-2041fea3bcd
 */
 
-
-/* Iconify docs: 
-  https://github.com/iconify/iconify/tree/master/packages/vue
-  https://iconify.design/icon-sets/
-
-  npm install --save-dev @iconify-icons/brandico
-*/
-import { Icon } from '@iconify/vue';
-// import home from '@iconify-icons/mdi/home';
-import iconTwitter from '@iconify-icons/brandico/twitter-bird';
-import iconLinkedIn from '@iconify-icons/brandico/linkedin-rect';
-import iconEmail from '@iconify-icons/mdi/email';
-
 @Options({
   components: {
-    HelloWorld,
-    FFLogo,
-    Icon
-    // nput
+    ContactFF,
+    FFLogo
   },
 })
 export default class Home extends Vue {
     data() {
       return {
-        icons: {
-          twitter: iconTwitter,
-          linkedin: iconLinkedIn,
-          email: iconEmail
         }
       }
-    }
 
 }
 </script>
